4fc1881b2efabf5722b3446b2ac16ce913cd9280
Ethan Williams <ethanmw@swbell.net>
2026-02-20 22:15:57 -0600

    fix randomwall.sh - hyprpaper CLI breaking change

--- hypr/dot-config/hypr/randomwall.sh
+++ hypr/dot-config/hypr/randomwall.sh
@@ -7,7 +7,7 @@ CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 
 # Apply the selected wallpaper
-hyprctl hyprpaper reload ,"$WALLPAPER"
+hyprctl hyprpaper wallpaper ,"$WALLPAPER"
 
 # Set a permanent symlink
 ln -sf $WALLPAPER ~/.config/media/currentwallpaper 
